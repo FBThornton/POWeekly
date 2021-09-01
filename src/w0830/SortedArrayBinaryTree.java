e1fd61ff7dc1fce5b4c9e1b7085889381dd8686f
FBThornton <97fthornton@gmail.com>
2021-09-01 14:09:34 -0400

    Weekly challenge upload

--- src/w0830/SortedArrayBinaryTree.java
+++ src/w0830/SortedArrayBinaryTree.java
@@ -1,5 +1,36 @@
 package w0830;
 
-public class SortedArrayBinaryTree {
+import java.util.Arrays;
 
+public class SortedArrayBinaryTree {
+	
+	public class Node {
+		int value;
+		Node left;
+		Node right;
+		
+		public Node() {
+		}
+		
+		public Node(int value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+		
+		public Node(int value, Node left, Node right) {
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+		
+		public Node arrayToTree(int[] arr) {
+			int middle = arr.length / 2;
+			if(middle == 0) {
+				return new Node(arr[middle]);
+			} else {
+				return new Node(arr[middle], arrayToTree(Arrays.copyOfRange(arr, 0, middle - 1)), arrayToTree(Arrays.copyOfRange(arr, middle + 1, arr.length)));
+			}
+		}
+	}
 }
