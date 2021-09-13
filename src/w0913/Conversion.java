bd4c4d61532cf18f38ec8648d1fb2e03330a3e42
FBThornton <97fthornton@gmail.com>
2021-09-13 14:39:55 -0400

    Weekly challenge completed

---
+++ src/w0913/Conversion.java
@@ -0,0 +1,25 @@
+package w0913;
+
+public class Conversion {
+
+	public String rot13Convert(String message) {
+		String encrypted = "";
+		
+		for (char c : message.toLowerCase().toCharArray()) {
+			if (Character.isLetter(c)) {
+				c += 13;
+				if(c > 122) {
+					c -= 26;
+				}
+			}
+			encrypted += c;
+		}
+		
+		return encrypted;
+	}
+	
+	public long octalConvert(long decNum) {
+		
+	}
+	
+}
